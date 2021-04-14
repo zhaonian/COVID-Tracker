3002ef638a48f64af3e31c00e1d9ac1d3f1d7e3b
Andrea <a.ocamp3@gmail.com>
2021-04-14 17:59:14 +0000

    added contants, map and heat map

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,14 +1,35 @@
 function createMap() {
-    console.log("map here");
+    //double lat[] = {41.881832, 34.052235, 48.864716};
+   // double long[] = {-87.623177, 34.052235, 48.864716};
+    //ArrayList<String[]> list = new ArrayList<String[]>();
+    list_lat_long = [[37.420947,-122.095643], [37.357049,-121.939285], [37.40299,-122.079401]];
+    var test = list_lat_long[0]; //test is the first array in the list 
+    //console.log("my test!!");
+    //console.log(test[0]);
+    //console.log(test[1]);
+    //console.log(list_lat_long.length);
+    //console.log(test.length);
     const uluru = { lat: 41.8807, lng: -87.6742 };
+    const centerUS = { lat: 39.8283, lng: -98.5795 };
     const map = new google.maps.Map(document.getElementById('map'),
-      {center: uluru,
-       zoom: 16});
+      {center: centerUS,
+       zoom: 3.5});
        // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
+        const marker = new google.maps.Marker({
         position: uluru,
         map: map,
     })
+    //test loop for many markers
+    for(i =0; i<list_lat_long.length ; i++){
+        var temp = list_lat_long[i];
+        //for(j= 0; j< test.length, j++){
+            var uluruTEST={ lat: temp[0], lng: temp[1]}
+            const marker = new google.maps.Marker({
+            position: uluruTEST,
+            map: map,
+    })
+        //}
+    }
 }
 
 /**fetches coviddata from api and adds to DOM */
