1c52b6f787e38e81523702fc40d91f7f83e05a8d
Andrea Ocampo <42876702+aanddrea@users.noreply.github.com>
2021-04-15 19:15:10 -0500

    Feat/ma (#7)
    * add google map api
    
    * added contants, map and heat map

---
+++ src/main/java/com/google/sps/servlets/HeatMapServlet.java
@@ -0,0 +1,22 @@
+package com.google.sps.servlets;
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+/** receives a request and redirects user to data.html */
+@WebServlet("/heatMap")
+public class HeatMapServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+       
+        response.sendRedirect("/heatmap.html");
+    }
+}
