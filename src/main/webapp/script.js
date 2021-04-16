e1c0f3f4eb02fef08c6636db3e38efa5b3a2104a
Andrea <a.ocamp3@gmail.com>
2021-04-16 02:16:26 +0000

    map things

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,35 +1,21 @@
 function createMap() {
-    //double lat[] = {41.881832, 34.052235, 48.864716};
-   // double long[] = {-87.623177, 34.052235, 48.864716};
-    //ArrayList<String[]> list = new ArrayList<String[]>();
+    console.log("map here");    
     list_lat_long = [[37.420947,-122.095643], [37.357049,-121.939285], [37.40299,-122.079401]];
     var test = list_lat_long[0]; //test is the first array in the list 
-    //console.log("my test!!");
-    //console.log(test[0]);
-    //console.log(test[1]);
-    //console.log(list_lat_long.length);
-    //console.log(test.length);
-    const uluru = { lat: 41.8807, lng: -87.6742 };
+   
     const centerUS = { lat: 39.8283, lng: -98.5795 };
     const map = new google.maps.Map(document.getElementById('map'),
       {center: centerUS,
        zoom: 3.5});
-       // The marker, positioned at Uluru
-        const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
-    })
-    //test loop for many markers
-    for(i =0; i<list_lat_long.length ; i++){
-        var temp = list_lat_long[i];
-        //for(j= 0; j< test.length, j++){
-            var uluruTEST={ lat: temp[0], lng: temp[1]}
-            const marker = new google.maps.Marker({
-            position: uluruTEST,
-            map: map,
-    })
-        //}
-    }
+       
+    // for(i =0; i<list_lat_long.length ; i++){
+    //     var temp = list_lat_long[i];
+    //         var uluruTEST={ lat: temp[0], lng: temp[1]}
+    //         const marker = new google.maps.Marker({
+    //         position: uluruTEST,
+    //         map: map,
+    // })
+    // }
 }
 
 /**fetches coviddata from api and adds to DOM */
@@ -59,12 +45,13 @@ async function getString() {
 
 /**fetches coviddata from api to access vaccine info and adds to DOM */
 async function loadVaccineSearchBarData() {
-  const state = await getString();  
+  const state = await getString(); 
+  console.log(state);
   const appended = false;
   fetch('https://api.covidactnow.org/v2/states.json?apiKey=4ac37661a08a40b5a50cbe35eb941043').then(response => response.json()).then((vaccine) => {
     const vaccineElement = document.getElementById('vaccine-container');
     vaccine.forEach((ind_vaccine) => {
-        if(state.localeCompare(ind_vaccine.state) == 0){
+         if(state.localeCompare(ind_vaccine.state) == 0){
             vaccineElement.appendChild(createVaccineElement(ind_vaccine));
             var linebreak = document.createElement("br");
             vaccineElement.appendChild(linebreak);
@@ -72,13 +59,28 @@ async function loadVaccineSearchBarData() {
             appended = true;
         }
      })
-    const errorElement = document.createElement('span');
-    errorElement.innerText = "invalid input, enter state as abbreveations : See table below ";
-    vaccineElement.append(errorElement);
-
-    const img = document.createElement("img");
-    img.src = '/images/state abbreviation.jpeg' ;
-    vaccineElement.appendChild(img);
+     if(state.localeCompare('CHICAGO')== 0){
+        console.log(state);
+        const centerUS = { lat: 39.8283, lng: -98.5795 };
+        const map = new google.maps.Map(document.getElementById('map'),
+        {center: centerUS,
+        zoom: 10});
+        const uluru = { lat: 41.8807, lng: -87.6742 };
+        const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,        
+    })
+     }
+     else{
+        const errorElement = document.createElement('span');
+        errorElement.innerText = "invalid input, enter state as abbreveations : See table below ";
+        vaccineElement.append(errorElement);
+        const img = document.createElement("img");
+        img.src = '/images/state abbreviation.jpeg' ;
+        vaccineElement.appendChild(img);
+     }
+        
+   
   });
 }
 
